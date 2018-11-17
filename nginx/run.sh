3aadbf2b735b3dad5aa2e850aa4149b2effb050b
eddie <zhengchangshuai@mobike.com>
2018-11-17 10:18:44 +0800

    update

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,4 +2,13 @@ cur=$(pwd)
 docker stop nginx
 docker rm nginx
 rm -f /opt/logs/nginx/*
-docker run --name nginx -p 80:80 -p 443:443 -v $cur/etc/nginx:/etc/nginx -v /opt/logs/nginx:/var/log/nginx/ -v /etc/letsencrypt/:/etc/letsencrypt/ -v /etc/localtime:/etc/localtime -d nginx:latest 
+
+with_time="-v /etc/localtime:/etc/localtime:ro"
+wth_key="-v /etc/letsencrypt/"
+os=$(uname)
+if [ "-$os" = "-Darwin" ]; then
+    with_time=""
+    with_key="-v $cur/letsencrypt/"
+fi
+
+docker run --name nginx -p 80:80 -p 443:443 -v $cur/etc/nginx:/etc/nginx -v /opt/logs/nginx:/var/log/nginx/ -v /etc/letsencrypt/:/etc/letsencrypt/ $with_time $with_key -d nginx:latest 
